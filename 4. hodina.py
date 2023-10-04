bc71862e9c8887b916abfba77aed46c81d010a1d
Ivar35 <144232018+Ivar35@users.noreply.github.com>
2023-10-04 12:01:26 +0000

    4. hodina

---
+++ 4. hodina.py
@@ -0,0 +1,59 @@
+# print("Hi, write sklenice, I will praise you.")
+
+
+#myinput = input("here: ")
+#if myinput == 5:
+#    print("GL")
+
+
+"""number1 = int(input("Write first number"))
+number2 = int(input("Write second number"))
+if number1 > number2:
+    print(number1)
+    print("je větší než")
+    print(number2)"""
+
+
+# Napsat sklenice
+#srovnat
+#pokud správně, pochválit se
+#pokud špatně, napsat zkus znovu
+
+"""
+print("Hi, write glass, I will praise you." )
+
+
+while True:
+    mujinput = input("Here: ")
+
+
+    if mujinput == "glass":
+        print("Nice job, congratilations.")
+        break
+    else :
+        print("Incorrect, try again.")"""
+
+
+
+
+
+print("Tom, Ivar, Seba, Adam, Ela")
+
+favourite_teacher = input("Ted tady je ta otázka, kdo je oblíbený učitel")
+
+
+print("Tvůj oblíbený učitel je")
+print(favourite_teacher)
+
+
+if favourite_teacher == "Šmahaj" :
+    print("Můj taky.") 
+
+elif favourite_teacher == "Turnovský" :
+    print("Můj ne") 
+
+elif favourite_teacher == "Soulek" :
+    print("Můj tal¨ka třeba nao.") 
+
+else:
+    print("Jseš magor")
